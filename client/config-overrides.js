22ce0a1ed1efc3ff8a6dca293f3de36cb16a94f7
Fernando Gomez <gomezhyuuga@gmail.com>
2018-05-17 15:59:09 -0500

    UI: Transaction List with headers

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,9 +1,8 @@
-const {injectBabelPlugin} = require('react-app-rewired')
+const {injectBabelPlugin}  = require('react-app-rewired')
 const rewireReactHotLoader = require('react-app-rewire-hot-loader')
 
 module.exports = function override(config, env) {
     config = injectBabelPlugin([ 'import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' } ], config);
-    // config = injectBabelPlugin("transform-optional-chaining", config);
     config = rewireReactHotLoader(config, env);
     return config;
 }
--- client/src/components/TransactionList.jsx
+++ client/src/components/TransactionList.jsx
@@ -1,18 +1,17 @@
+import _ from 'lodash'
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import TransactionListItem from './TransactionListItem'
 import {List, Button, Icon, Timeline, Row, Col} from 'antd'
 import formatColumn from 'accounting-js/lib/formatColumn'
+import { format } from 'date-fns'
+import TransactionListItem from './TransactionListItem'
 
 class TransactionList extends Component {
-    onClick = (transaction) => {
+    onClick(transaction) {
         if (this.props.onShowDetails) this.props.onShowDetails(transaction);
     }
-    render() {
-        const {transactions} = this.props;
-        const amounts = formatColumn(transactions.map(t => t.amount));
-
-        const items = transactions.map((t, i) => {
+    _build_items(transactions, amounts) {
+        return transactions.map((t, i) => {
             let options = { color: 'blue' }
             if (t.recurring) {
                 options.color = 'red';
@@ -22,12 +21,8 @@ class TransactionList extends Component {
             return <Timeline.Item style={{background: 'none'}} key={t.transaction_id} {...options}>
                     <a onClick={this.onClick.bind(this, t)} className='timeline-item'>
                         <Row>
-                            <Col span={4}>
-                                <small>{t.date}</small>
-                            </Col>
-                            <Col span={12}>
-                                {t.name}
-                            </Col>
+                            <Col span={4}><small>{t.date}</small></Col>
+                            <Col span={12}>{t.name}</Col>
                             <Col span={4} style={{verticalAlign: 'middle'}}>
                                 <pre style={{margin: 0}}>{amounts[i]}</pre>
                             </Col>
@@ -36,7 +31,23 @@ class TransactionList extends Component {
                             </Col>
                         </Row>
                     </a>
-                </Timeline.Item>});
+                </Timeline.Item>;
+        });
+    }
+    render() {
+        const {transactions} = this.props;
+        const amounts = formatColumn(transactions.map(t => t.amount));
+
+        const items = _(transactions)
+                        .groupBy('date')
+                        .map( (lst, date) => {
+            const date_header = <Timeline.Item className='timeline-header'
+                                    dot={<Icon type='calendar' />}>
+                                        {format(date, 'PPPP')}
+                                    </Timeline.Item>;
+            return [date_header, ...this._build_items(lst, amounts)];
+        }).value();
+
         return  <React.Fragment>
                     <h2>Transactions <small>({items.length})</small></h2>
                     <Timeline>{items}</Timeline>
